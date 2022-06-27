1fb4087aa0d894c1f502dfa575291665ee161dad
wukaiwei1 <2596862515@qq.com>
2022-06-27 19:42:34 +0800

    下载依赖包，处理css和less文件问题

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@
 import './banner'
 // 引入tab栏切换js文件的逻辑
 import './tabs'
+
+// 引入css文件
+import './style/index.css'
+
+// 引入less文件
+import './style/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,26 @@ module.exports = {
     devServer: {
         port: 3000, // 端口号
         open: true
+    },
+    // loader 加载器配置
+    module: {
+        rules: [ // loader的规则
+            {
+                test: /\.css$/, // 匹配所有的css文件
+                // loader 执行的顺序： use数组里从右向左运行
+                // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+                // 再用 style-loader 将样式, 把css插入到dom中
+                use: ["style-loader", "css-loader"]
+            },
+            { // 配置处理less
+                test: /\.less$/, // 匹配以 less结尾的文件
+                // css-loader 把css 文件转换成 webpack 可以识别的文件
+                // style-loader 把css代码 插入到dom中
+                // loader 执行 从右往左
+                // less-loader 将less 文件转换成 webpack可以识别的代码
+                // less 讲 less 语法 转换成 css
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+        ]
     }
 }
